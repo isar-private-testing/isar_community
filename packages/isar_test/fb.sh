bcf592799b7948bbacdc54ebbd1855c3903386c5
Vicente Russo <vicente.russo@gmail.com>
2025-07-31 22:19:20 -0300

    w

--- packages/isar_test/fb.sh
+++ packages/isar_test/fb.sh
@@ -5,4 +5,4 @@ fvm flutter clean && fvm flutter packages pub get && fvm flutter pub run build_r
 fvm dart tool/generate_long_double_test.dart
 fvm dart tool/generate_all_tests.dart
 
-fvm flutter pub run build_runner build
+fvm flutter pub run build_runner build --delete-conflicting-outputs
