8a005b62ad1aa5a05a5e5b8dee569aff6a9229fa
Vicente Russo <vicente.russo@gmail.com>
2025-08-06 23:49:50 -0300

    aarch64-unknown-linux-gnu

--- tool/build_linux.sh
+++ tool/build_linux.sh
@@ -3,7 +3,7 @@ if [ "$1" = "x64" ]; then
   rustup run 1.88.0-x86_64-unknown-linux-gnu cargo build --target x86_64-unknown-linux-gnu --release
   mv "target/x86_64-unknown-linux-gnu/release/libisar.so" "libisar_linux_x64.so"
 else
-  rustup target add aarch64-unknown-linux-gnu
-  cargo build --target aarch64-unknown-linux-gnu --release
+  rustup target add aarch64-unknown-linux-gnu --toolchain 1.88.0-aarch64-unknown-linux-gnu
+  rustup run 1.88.0-aarch64-unknown-linux-gnu cargo build --target aarch64-unknown-linux-gnu --release
   mv "target/aarch64-unknown-linux-gnu/release/libisar.so" "libisar_linux_arm64.so"
 fi
