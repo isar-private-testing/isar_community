67f554926e870dc75c7ee8bc5842eb1f0511f9b9
Vicente Russo <vicente.russo@gmail.com>
2025-08-10 23:52:07 -0300

    Updated isar_test package to reflect new flutter android project structure

--- packages/isar_test/android/app/src/androidTest/java/com/isar_community/isar_test/MainActivityTest.java
+++ packages/isar_test/android/app/src/androidTest/java/com/isar_community/isar_test/MainActivityTest.java
@@ -1,4 +1,4 @@
-package dev.isar.isar_test;
+package com.isar_community.isar_test;
 
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
@@ -8,5 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
   @Rule
-  public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
+  public ActivityTestRule<MainActivity> rule =
+      new ActivityTestRule<>(MainActivity.class, true, false);
 }
+
+
---
+++ packages/isar_test/android/app/src/main/kotlin/com/example/isar_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.isar_community.isar_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
